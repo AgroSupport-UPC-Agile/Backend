ed8fe6684ffcb8ba7612cf135b348bb9a00e8e3e
Alexander Jair Castillo <142842509+U202211390@users.noreply.github.com>
2025-05-24 16:40:51 -0500

    add: added new steps for user stories

--- agrosupport/src/test/java/steps/US02Steps.java
+++ agrosupport/src/test/java/steps/US02Steps.java
@@ -1,4 +1,56 @@
 package steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.And;
+import java.util.*;
+
 public class US02Steps {
+    
+    private String granjero;
+    private String asesorSeleccionado;
+    private String informacion;
+    private int errorCode;
+    private String mensajeError;
+
+    @Given("el {string} con poca experiencia quiere ver {string} de un asesor.")
+    public void el_granjero_con_poca_experiencia_quiere_ver_informacion(String granjero, String informacion) {
+        this.granjero = granjero;
+        this.informacion = informacion;
+        // Simular que el granjero quiere ver información
+    }
+
+    @And("se encuentra en el apartado del \"Catálogo de Asesores\" en su dispositivo móvil.")
+    public void se_encuentra_en_el_apartado_de_catalogo_asesores() {
+        // Simular que está en el catálogo de asesores
+    }
+
+    @When("seleccione al {string} de un asesor en la interfaz móvil.")
+    public void seleccione_al_cuadro_de_un_asesor(String asesor) {
+        this.asesorSeleccionado = asesor;
+        // Simular selección del asesor
+    }
+
+    @Then("el sistema le mostrará la {string} del asesor como nombre, experiencia, calificación y reseñas de manera optimizada para móvil.")
+    public void el_sistema_le_mostrara_la_informacion_del_asesor(String informacion) {
+        // Verificar que se muestra la información correcta
+    }
+
+    @And("se encuentra en el apartado de \"Asesores\" en su dispositivo móvil.")
+    public void se_encuentra_en_el_apartado_de_asesores() {
+        // Simular que está en el apartado de asesores
+    }
+
+    @And("se encuentre con un {int} al cargar la información.")
+    public void se_encuentre_con_un_error_al_cargar_informacion(Integer errorCode) {
+        this.errorCode = errorCode;
+        // Simular error al cargar información
+    }
+
+    @Then("el sistema le mostrará un {string} de error de carga en la interfaz móvil.")
+    public void el_sistema_le_mostrara_un_mensaje_de_error(String mensaje) {
+        this.mensajeError = mensaje;
+        // Verificar que se muestra el mensaje de error
+    }
 }
--- agrosupport/src/test/java/steps/US04Steps.java
+++ agrosupport/src/test/java/steps/US04Steps.java
@@ -1,4 +1,70 @@
 package steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.And;
+import java.util.*;
+
 public class US04Steps {
+    
+    private String granjero;
+    private String asesor;
+    private String horarioDisponible;
+    private String boton;
+    private String mensajeConfirmacion;
+    private int errorCode;
+    private String mensajeError;
+    
+    @Given("el {string} con poca experiencia desea programar una cita.")
+    public void el_granjero_con_poca_experiencia_desea_programar_cita(String granjero) {
+        this.granjero = granjero;
+        // Simular que el granjero desea programar una cita
+    }
+    
+    @And("se encuentra en el apartado de \"Horarios Disponibles\" del perfil de un {string} en su dispositivo móvil.")
+    public void se_encuentra_en_apartado_horarios_disponibles(String asesor) {
+        this.asesor = asesor;
+        // Simular que está en el apartado de horarios disponibles
+    }
+    
+    @When("seleccione un {string}.")
+    public void seleccione_un_horario_disponible(String horario) {
+        this.horarioDisponible = horario;
+        // Simular selección de horario
+    }
+    
+    @And("complete los campos solicitados.")
+    public void complete_los_campos_solicitados() {
+        // Simular completar campos
+    }
+    
+    @And("haga clic en el {string} \"Reservar Cita\".")
+    public void haga_clic_en_boton_reservar_cita(String boton) {
+        this.boton = boton;
+        // Simular clic en botón
+    }
+    
+    @Then("el sistema le mostrará un {string} de confirmación.")
+    public void sistema_mostrara_mensaje_confirmacion(String mensaje) {
+        this.mensajeConfirmacion = mensaje;
+        // Verificar que se muestra el mensaje de confirmación
+    }
+    
+    @And("se encuentra en el apartado de \"Horarios Disponibles\" del perfil de un asesor.")
+    public void se_encuentra_en_apartado_horarios_disponibles_asesor() {
+        // Simular que está en el apartado de horarios disponibles
+    }
+    
+    @And("se encuentra un {int} técnico o de conexión que impide completar la programación.")
+    public void se_encuentra_un_error_tecnico(Integer error) {
+        this.errorCode = error;
+        // Simular error técnico
+    }
+    
+    @Then("el sistema le mostrará un {string} de error.")
+    public void sistema_mostrara_mensaje_error(String mensaje) {
+        this.mensajeError = mensaje;
+        // Verificar que se muestra el mensaje de error
+    }
 }
